7e517a3d17c16460d58b6a420e3691b9855518b9
Devlin Hitchcock <devlinhitchcock0@gmail.com>
2018-12-24 12:29:38 +0200

    Added annotations for identifying spring beans and configuring them automatically

--- dijkstra-web/src/main/java/za/co/dmh/Application.java
+++ dijkstra-web/src/main/java/za/co/dmh/Application.java
@@ -1,9 +1,13 @@
 package za.co.dmh;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@EnableAutoConfiguration
+@ComponentScan(basePackages = "za.co.dmh")
 public class Application {
 
     public static void main(String[] args) {
