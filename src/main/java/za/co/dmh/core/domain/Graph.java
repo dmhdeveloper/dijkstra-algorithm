50d3074ffd808de69a36ac6edfbbf6db07db594f
Devlin Hitchcock <devlinhitchcock0@gmail.com>
2018-12-31 09:05:25 +0200

    Added initial Graph object to be used by Dijkstras Algorithm, needs updating

---
+++ src/main/java/za/co/dmh/core/domain/Graph.java
@@ -0,0 +1,25 @@
+package za.co.dmh.core.domain;
+
+import java.util.List;
+
+public class Graph {
+
+    List<Node> nodeList;
+    List<Route> routeList;
+
+    public List<Node> getNodeList() {
+        return nodeList;
+    }
+
+    public void setNodeList(List<Node> nodeList) {
+        this.nodeList = nodeList;
+    }
+
+    public List<Route> getRouteList() {
+        return routeList;
+    }
+
+    public void setRouteList(List<Route> routeList) {
+        this.routeList = routeList;
+    }
+}
